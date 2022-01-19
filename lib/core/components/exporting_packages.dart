46099149a28e0cc1f6688668d4a42fd27020bb03
BahromjonPolat <itjunior.uz@gmail.com>
2022-01-19 21:09:34 +0500

    SizeConfig, CustomNavigator va Exporting packages

---
+++ lib/core/components/exporting_packages.dart
@@ -0,0 +1,22 @@
+
+// Screens
+export 'package:adhan/view/screens/home/home_page_view.dart';
+
+// Constants
+export 'package:adhan/core/constants/asset_icons.dart';
+
+// Components
+export 'package:adhan/core/components/custom_navigator.dart';
+export 'package:adhan/core/components/size_config.dart';
+
+// Widgets
+export 'package:adhan/view/widgets/custom_bottom_navigation_bar.dart';
+
+// Provider
+export 'package:adhan/provider/bottom_nav_bar_provider.dart';
+
+// Services
+export 'package:flutter/services.dart';
+
+
+
